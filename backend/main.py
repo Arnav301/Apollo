b25ba7f73894ad0f1577668e73c496fef73cba7f
Arnav301 <arnavverma3016@gmail.com>
2025-09-19 11:25:41 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -34,3 +34,7 @@ app.include_router(chat.router)
 @app.get("/")
 def root():
     return {"message": "✅ Apollo Backend is running with MongoDB!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 9000))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=True)
